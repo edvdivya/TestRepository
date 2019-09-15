63358c030c010fb1405cd2256f6670e6b8b39c3f
DAK <DAK@DAKSH>
2019-09-15 15:55:49 +0530

    A home test reporitory

---
+++ HomeTest/src/main/java/com/hometest/demo/ui/MyApplication.java
@@ -0,0 +1,11 @@
+package com.hometest.demo.ui;
+
+public class MyApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("heyyy will this work");
+	}
+
+}
